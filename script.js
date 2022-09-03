0be327d9525f90d481318334524ee37541cbcd2c
hamidreza <takloohamidreza@gmail.com>
2022-09-03 12:26:57 +0430

    Return subtract of two number

--- script.js
+++ script.js
@@ -8,5 +8,9 @@ document.addEventListener('click',(e)=>{
 })
 
 const add =function(num1,num2){
-    return (num1+num2)
+    return (num1+num2);
+}
+
+const subtract = function(num1,num2){
+    return(num1-num2);
 }
